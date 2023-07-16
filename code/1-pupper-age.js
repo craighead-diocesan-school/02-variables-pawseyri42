5007c63918edbe566917354a5f35479bedb2d98f
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-07-16 21:52:14 +0000

    dog years fixed

--- code/1-pupper-age.js
+++ code/1-pupper-age.js
@@ -6,7 +6,7 @@ function pupperAge() {
   alert('Dog years are human years times seven.')
 
   let humanYears = prompt('How old is your dog in human years?')
-let years = prompt() 
-let dogYears = 7 * years
+
+let dogYears = 7 * humanYears
  alert ( "Your dog is " + dogYears + " years old " )
 }
