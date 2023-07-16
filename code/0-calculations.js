975723d468fda66d25fafa0ba4c2a32317e6509c
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-07-16 21:59:03 +0000

    calculations done

--- code/0-calculations.js
+++ code/0-calculations.js
@@ -2,4 +2,27 @@
 // #### ----- Calculations ----- ######
 // ####################################
 
-function calculations() {}
+function calculations() {
+alert( 8 + 2 )
+alert( 8 - 2 )
+alert( 8 * 2 )
+alert( 10 / 4 )
+
+let firstNumber = 20 
+let secondNumber = 5 
+
+let result = firstNumber * secondNumber 
+
+alert (result)
+
+firstNumber = prompt ('Enter a number')
+
+alert(result)
+
+ firstNumber = prompt('Enter a number')
+ secondNumber = 2
+
+ result = first_number * second_number
+
+ alert (firstnumber + ' * ' + secondNumber + ' = ' + result)
+}
