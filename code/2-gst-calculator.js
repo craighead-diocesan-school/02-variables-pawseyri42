2f4f70f1f819fb413768e565301c243fb85c69aa
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-07-17 07:10:43 +0000

    GST done

--- code/2-gst-calculator.js
+++ code/2-gst-calculator.js
@@ -4,4 +4,9 @@
 
 function gstCalculator() {
   alert('The price with GST the price and 15%.')
+
+ let price = prompt (' what is the price before GST? ')
+
+ let gstPrice = 0.15 * price
+alert ('The price with gst is now ' + gstPrice )
 }
