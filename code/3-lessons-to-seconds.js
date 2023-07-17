5ee6a1247bef0880de2e3fc3f31f4374c9d0bd44
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-07-17 07:16:04 +0000

    seconds left of lessons finished

--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,11 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+
+  let lessonAmount = prompt (' How many lessons do you have left today? ')
+
+  let secondsLeft = 3600 * lessonAmount
+
+  alert ( ' You have ' + secondsLeft + ' seconds left of lessons 🥲' )
+
 }
